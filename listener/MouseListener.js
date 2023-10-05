107cef32e69e490519d8f7dd43729e90c78b9192
Adam <atmostadam@gmail.com>
2023-10-05 07:06:01 -0400

    Bug Fix

--- listener/MouseListener.js
+++ listener/MouseListener.js
@@ -7,7 +7,7 @@ export class MouseListener {
             const rect = GameContext.getCanvas().getBoundingClientRect();
             const x = e.clientX - rect.left;
             const y = e.clientY - rect.top;
-            Log.info("User clicked [" + x + ", " + y + "] so run onClick forEach " + GameContext.getClickSubscribers());
+            Log.info("User clicked [" + x + ", " + y + "] so run onClick forEach " + GameContext.getClickSubscribers(), this);
             GameContext.getClickSubscribers().forEach(c => { c.onClick(x, y); });
         });
     }
